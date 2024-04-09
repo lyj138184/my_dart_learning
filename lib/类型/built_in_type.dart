a3ef8bdfabe15b59f1f68c7ecd5e42f9fe0aa590
lyj138184 雷 <138184138@qq.com>
2024-04-09 09:28:08 +0000

    类型

---
+++ lib/类型/built_in_type.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:characters/characters.dart';
+void main(List<String> args) {
+/*   //num变量同时具有整数值和双精度
+  num x = 1;
+  x += 2.5;
+
+  //字符串转化为数字，反之
+  var one = int.parse('1');
+  assert(one == 1);
+
+// String -> double
+  var onePointOne = double.parse('1.1');
+  assert(onePointOne == 1.1);
+
+// int -> String
+  String oneAsString = 1.toString();
+  assert(oneAsString == '1');
+
+// double -> String
+  String piAsString = 3.14159.toStringAsFixed(2);
+  assert(piAsString == '3.14'); */
+/*   var s1 = 'String '
+      'concatenation'
+      " works even over line breaks.";
+  assert(s1 ==
+      'String concatenation works even over '
+          'line breaks.');
+
+  var s2 = 'The + operator ' + 'works, as well.';
+  print(s2 == 'The + operator works, as well.');
+
+  //创建多行字符串
+  var s11 = '''
+           You can create
+           multi-line strings like this one.
+           ''';
+
+  var s22 = """This is also a
+             multi-line string.""";
+
+  print(s22);
+
+  var s = r'In a raw string, not even \n gets special treatment.';
+  print(s);
+
+  var s4 = 's444444444';
+  print(s4); */
+
+  /* var s = '2';
+  print(s.runtimeType);
+  var s2 = int.parse(s);
+  print(s2.runtimeType); */
+  
+  // Check for an empty string.
+  /* var fullName = '';
+  assert(fullName.isEmpty);
+
+  var hitPoints = 0;
+  assert(hitPoints <= 0);
+
+  var unicorn = null;
+  assert(unicorn == null);
+
+  var iMeantToDoThis = 0 / 0;
+  assert(iMeantToDoThis.isNaN); */
+
+  /* var hi = 'Hi 🇩🇰';
+  print(hi);
+  print('The end of the string: ${hi.substring(hi.length - 1)}');
+  print('The last character: ${hi.characters.last}'); */
+
+
+  
+}
